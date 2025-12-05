62aae2cd923e763f2cffb19e25c6a36b9343d7e9
Dadens Julianto <131245562+DadenJulianto@users.noreply.github.com>
2025-12-05 09:25:56 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -167,7 +167,7 @@ export default function Home() {
                 onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.9")}
                 onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
               >
-                Lihat Program Kami
+                Brosur
               </button>
             </div>
             <div className="w-full h-96 rounded-lg overflow-hidden shadow-lg">
